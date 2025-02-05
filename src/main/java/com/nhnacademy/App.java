f745c4fab696018130c0b9fb1319ebbe8b81d1d0
Xtra <xtra72@gmail.com>
2025-02-05 13:32:14 +0900

    문제 작성 완료

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -12,14 +12,39 @@
 
 package com.nhnacademy;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Echo Server
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void echoHandler(Socket socket) {
+        try (/* TODO:  소켓에서 InputStream/OutputStream을 얻어내 수신 및 송신에 적용합니다. */) {
+
+            while (!Thread.currentThread().isInterrupted()) {
+                // TODO: nextLine을 이용해 줄 문자열을 받습니다.
+                // TODO: 받은 문자열은 돌려 보냅니다.
+            }
+
+        } catch (IOException e) {
+            System.err.println("데이터 수신중 오류가 발생하였습니다: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 12345;
+        try (/* TODO: 서버 소켓을 생성합니다. */
+             /* TODO: 서버 소켓 객체의 accept() 메소드를 이용해 접속을 기다리며, 클라이언트 접속시 반환되는 소켓을 이용합니다. */) {
+
+            echoHandler(socket);
+        } catch (IOException e) {
+            System.err.println("연결에 오류가 발생하였습니다: " + e.getMessage());
+        }
     }
 }
