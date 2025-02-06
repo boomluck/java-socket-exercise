9936589311c31872d248516a625a9ee4ac90201d
Xtra <xtra72@gmail.com>
2025-02-06 12:25:30 +0900

    문제 설정

--- src/main/java/com/nhnacademy/EchoServer.java
+++ src/main/java/com/nhnacademy/EchoServer.java
@@ -29,13 +29,13 @@ public class EchoServer {
         }
 
         // TODO#1-1: 소켓에서 InputStream/OutputStream을 얻어내 수신 및 송신에 적용합니다
-        try (Scanner socketIn = new Scanner(socket.getInputStream());
-                PrintStream socketOut = new PrintStream(socket.getOutputStream())) {
+        try (Scanner socketIn = null;
+                PrintStream socketOut = null) {
 
             while (!Thread.currentThread().isInterrupted()) {
                 System.out.println("메시지를 기다립니다.");
                 // TODO#1-2: nextLine을 이용해 문자열을 받습니다.
-                String line = socketIn.nextLine();
+                String line = null;
                 if (line.isEmpty()) {
                     System.out.println("빈 메시지를 받았습니다. 연결을 끊고, 프로그램을 종료합니다.");
                     break;
@@ -43,7 +43,6 @@ public class EchoServer {
                 System.out.print("메시지를 받았습니다: ");
                 System.out.println(line);
                 // TODO#1-3: 받은 문자열은 돌려 보냅니다.
-                socketOut.println(line);
                 System.out.println("메시지를 보냈습니다.");
             }
 
@@ -56,10 +55,10 @@ public class EchoServer {
         int port = 12345;
 
         // TODO#1-4: 서버 소켓을 생성합니다.
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try (ServerSocket serverSocket = null) {
             System.out.println("소켓을 생성하였습니다. 클라이언트 연결을 기다립니다.");
             // TODO#1-5: 서버 소켓 객체의 accept() 메소드를 이용해 접속을 기다리며, 클라이언트 접속시 반환되는 소켓을 이용합니다.
-            try (Socket socket = serverSocket.accept()) {
+            try (Socket socket = null) {
                 System.out.printf("클라이언트가 연결되었습니다.%n", socket.getInetAddress().getHostAddress(), socket.getPort());
                 echoHandler(socket);
             }
