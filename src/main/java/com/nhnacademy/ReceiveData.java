3682689e4f3501ad6d8dbe7dabbaefd4987ab9b1
Xtra <xtra72@gmail.com>
2025-02-06 12:35:02 +0900

    문제 설정

--- src/main/java/com/nhnacademy/ReceiveData.java
+++ src/main/java/com/nhnacademy/ReceiveData.java
@@ -23,14 +23,14 @@ public class ReceiveData {
         // TODO#1-1: 소켓을 try-with-resources로 생성합니다.
         // TODO#1-2: 수신을 위한 Scanner을 생성합니다. Scanner에 사용할 InputStream은 소켓에서
         // 얻어옵니다.
-        try (Socket socket = new Socket(host, port);
-                Scanner socketIn = new Scanner(socket.getInputStream())) {
+        try (Socket socket = null;
+                Scanner socketIn = null) {
             System.out.println("메시지 수신을 위한 소켓이 연결되었습니다.");
 
             while (!Thread.currentThread().isInterrupted()) {
                 System.out.println("메시지를 기다립니다.");
                 // TODO#1-3: 소켓을 통해 문자열을 받습니다.
-                String line = socketIn.nextLine();
+                String line = null;
                 // 빈 문자열이 오면 종료합니다.
                 if (line.isEmpty()) {
                     System.out.println("빈 메시지를 수신하여 종료합니다.");
