2cf34826aa8672612e5d03d12b75600c316686fc
Xtra <xtra72@gmail.com>
2025-02-07 21:54:08 +0900

    코드 작성 완료

--- src/main/java/com/nhnacademy/EchoHandler.java
+++ src/main/java/com/nhnacademy/EchoHandler.java
@@ -14,9 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 public class EchoHandler implements Runnable {
     static Map<Long, EchoHandler> handlerMap = new HashMap<>();
     Socket socket;
+    Scanner socketIn;
+    PrintStream socketOut;
 
-    public EchoHandler(Socket socket) {
+    public EchoHandler(Socket socket) throws IOException {
         this.socket = socket;
+        socketIn = new Scanner(socket.getInputStream());
+        socketOut = new PrintStream(socket.getOutputStream());
 
     }
 
@@ -25,9 +29,7 @@ public class EchoHandler implements Runnable {
         log.debug("응답을 시작합니다.");
         handlerMap.put(Thread.currentThread().threadId(), this);
 
-        try (Scanner socketIn = new Scanner(socket.getInputStream());
-                PrintStream socketOut = new PrintStream(socket.getOutputStream())) {
-
+        try {
             while (!Thread.currentThread().isInterrupted()) {
                 log.debug("메시지를 기다립니다.");
                 String line = socketIn.nextLine();
@@ -36,16 +38,26 @@ public class EchoHandler implements Runnable {
                     break;
                 }
                 log.debug("메시지를 받았습니다: {}", line);
-                socketOut.println(line);
+                broadcast(line);
                 log.debug("메시지를 보냈습니다.");
             }
         } catch (NoSuchElementException e) {
             log.debug("클라이언트에서 연결을 끊었습니다.");
-        } catch (IOException e) {
-            log.error("데이터 수신중 오류가 발생하였습니다: {}", e.getMessage());
         }
+
         log.debug("프로그램을 종료합니다.");
 
         handlerMap.remove(Thread.currentThread().threadId());
+
+        socketIn.close();
+        socketOut.close();
+    }
+
+    public void send(String message) {
+        socketOut.println(message);
+    }
+
+    static void broadcast(String message) {
+        handlerMap.forEach((id, handler) -> handler.send(message));
     }
 }
