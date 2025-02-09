cec4af0ddb08799196b38141fd3b3e4060e7e380
Xtra <xtra72@gmail.com>
2025-02-09 18:17:41 +0900

    문제 설정

--- src/main/java/com/nhnacademy/ReconnectableServer.java
+++ src/main/java/com/nhnacademy/ReconnectableServer.java
@@ -25,31 +25,12 @@ import java.util.Scanner;
  */
 public class ReconnectableServer {
     public static void echoHandler(Socket socket) {
-        if (socket == null) {
-            throw new IllegalArgumentException();
-        }
+        // TODO#1-1: 인수의 유효성의 검사합니다. 유효하지 않을 경우, IllegalArgumentException()을 발생시킵니다.
 
-        try (Scanner socketIn = new Scanner(socket.getInputStream());
-                PrintStream socketOut = new PrintStream(socket.getOutputStream())) {
-
-            while (!Thread.currentThread().isInterrupted()) {
-                System.out.println("메시지를 기다립니다.");
-                String line = socketIn.nextLine();
-                if (line.isEmpty()) {
-                    System.out.println("빈 메시지를 받았습니다. 연결을 끊습니다.");
-                    break;
-                }
-                System.out.print("메시지를 받았습니다: ");
-                System.out.println(line);
-                socketOut.println(line);
-                System.out.println("메시지를 보냈습니다.");
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("클라이언트에서 연결을 끊었습니다.");
-        } catch (IOException e) {
-            System.err.println("데이터 수신중 오류가 발생하였습니다: " + e.getMessage());
-        }
-        System.out.println("프로그램을 종료합니다.");
+        /*
+         * TOOD#1-2: 소켓에서 데이터 송수신을 위한 I/O Stream을 가져와 관련 객체를 생성하고,
+         * 클라이언트와 데이터를 주고 받습니다.
+         */
     }
 
     public static void main(String[] args) {
@@ -59,11 +40,11 @@ public class ReconnectableServer {
             System.out.println("소켓을 생성하였습니다.");
             while (!Thread.currentThread().isInterrupted()) {
                 System.out.println("클라이언트 연결을 기다립니다.");
-                try (Socket socket = serverSocket.accept()) {
-                    System.out.printf("클라이언트가 연결되었습니다.%n", socket.getInetAddress().getHostAddress(), socket.getPort());
-                    echoHandler(socket);
-                    System.out.println("클라이언트완 연결이 끊어졌습니다.");
-                }
+                /*
+                 * TODO#1-3: 클라이언트 접속을 기다리며, 접속하여 생성되는 소켓으로 메시지를 주고 받습니다.
+                 * 연결이 끊어지면, 다시 접속을 기다립니다.
+                 */
+
             }
         } catch (IOException e) {
             System.err.println("연결에 오류가 발생하였습니다: " + e.getMessage());
