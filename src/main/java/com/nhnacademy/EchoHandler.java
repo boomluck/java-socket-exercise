8b358aa6da005daac40628f5daf99a64171abf77
Xtra <xtra72@gmail.com>
2025-03-24 16:08:54 +0900

    문제 설정

--- src/main/java/com/nhnacademy/EchoHandler.java
+++ src/main/java/com/nhnacademy/EchoHandler.java
@@ -25,18 +25,20 @@ public class EchoHandler implements Runnable {
         log.debug("응답을 시작합니다.");
         handlerMap.put(Thread.currentThread().threadId(), this);
 
-        try (Scanner socketIn = new Scanner(socket.getInputStream());
-                PrintStream socketOut = new PrintStream(socket.getOutputStream())) {
+        // TODO#2: socket에서 데이터를 읽고 쓰기 위한 IOStream을 생성한다.
+        try (Scanner socketIn = null;
+                PrintStream socketOut = null) {
 
             while (!Thread.currentThread().isInterrupted()) {
                 log.debug("메시지를 기다립니다.");
-                String line = socketIn.nextLine();
+                // TODO#3: socket에서 데이터를 읽어들인다.
+                String line = null;
                 if (line.isEmpty()) {
                     log.debug("빈 메시지를 받았습니다. 연결을 끊습니다.");
                     break;
                 }
                 log.debug("메시지를 받았습니다: {}", line);
-                socketOut.println(line);
+                // TODO#4: 읽어 들인 메시지를 돌려 보낸다.
                 log.debug("메시지를 보냈습니다.");
             }
         } catch (NoSuchElementException e) {
