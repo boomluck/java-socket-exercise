cb07b8d6b6a023c3d215f51b43dde6724a34524e
{boomluck} <{boomluck@gmail.com}>
2026-02-13 17:48:46 +0900

    실행해 보았을 때 터미널 출력이 의도된 내용으로 출력되지 않아 1-11, 1-12, 1-13 수정.

--- src/main/java/com/nhnacademy/server/runable/MessageServer.java
+++ src/main/java/com/nhnacademy/server/runable/MessageServer.java
@@ -72,15 +72,21 @@ public class MessageServer implements Runnable {
 
                     // TODO#1-11 ResponseFactory를 이용해서 methodAndValue.getMethod()에 해당되는 response를
                     // 얻습니다.
-                    Response response = ResponseFactory.getResponse(methodAndValue.getMethod());
+                    Response response = null;
+                    try {
+                        response = ResponseFactory.getResponse(methodAndValue.getMethod());
+                    }catch (ResponseNotFoundException re){
+                        //ResponseNotFoundException 발생하면 로그로 남김니다.
+                        log.debug("response not found : {}", re.getMessage());
+                    }
 
                     String sendMessage;
                     if (Objects.nonNull(response)) {
                         // TODO#1-12 methodAndValue.getValue() 이용해서 response를 실행 합니다.
-                        sendMessage = methodAndValue.getValue();
+                        sendMessage = response.execute(methodAndValue.getValue());
                     } else {
                         // TODO#1-13 response가 null 이면 sendMessage를 "{echo} method not found" 로 설정 합니다.
-                        sendMessage = "{echo} method not found";
+                        sendMessage = String.format("{%s} method not found", methodAndValue.getMethod());
                     }
                     out.println(sendMessage);
                     out.flush();
